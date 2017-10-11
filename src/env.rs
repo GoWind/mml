b508c06ae29a4679e0449b27245b71f0eaf4be59
govindarajann <ngovind@protonmail.com>
2017-10-12 00:25:56 +0530

    Adding the interface and types for the Lisp types

--- src/env.rs
+++ src/env.rs
@@ -1,16 +1,23 @@
 use ast;
-pub enum ITypes {
-        Var(String),
-        Symbol(String),
+use std::collections::HashMap;
+pub enum IType {
+        Atom(String),
         Function,
-        Nil,
-        EmptyList
+        List(Vec<IType>),
+        True,
+        False,
+        Nil
 }
+
+
 
 
 static keywords: [&str; 6] =  ["define", "car", "cdr", "lambda", "nil", "empty-list"];
 
 
+pub fn interpret(env: &mut HashMap<String, IType>, form: ast::SExpType) -> HashMap<String, IType> {
+    HashMap::new()
+}
 
 /*
 pub fn interpret(forms: &Vec<ast::SExpType>) -> String {
