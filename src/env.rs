940a21f4479bdf8970448ebfd42b5c39eb9560d9
govindarajann <ngovind@protonmail.com>
2017-10-26 21:06:44 +0530

    added an initial implementation for eval

--- src/env.rs
+++ src/env.rs
@@ -1,6 +1,7 @@
 use ast;
 use std::collections::HashMap;
 use std::option;
+use std::result;
 pub enum IType {
         Atom(String),
         Function,
@@ -12,36 +13,67 @@ pub enum IType {
 
 pub enum Var {
     Bound(IType),
-    Unbound
+    UnBound
 }
 
 
-
-
-static keywords: [&str; 6] =  ["define", "car", "cdr", "lambda", "nil", "empty-list"];
+pub fn create_env() -> HashMap<String, IType> {
+    let mut k =  HashMap::new();
+    k
+}
 
 
 pub fn is_define(exp:  &ast::SExpType) -> bool {
-    match exp {
-        &ast::SExpType::Exp(ref form)  => {
-            match &form[0] {
-                &ast::SExpType::Identifier(ref name) => { *name == "define".to_string()}
-                _ => { false}
-            }
-        }
-        _ => {false}
-    }
+    get_first_term(exp) == "define"
 }
 
 pub fn is_car(exp : &ast::SExpType) -> bool {
-    match exp {
-        &ast::SExpType::Exp(ref form)  => {
-            match &form[0] {
-                &ast::SExpType::Identifier(ref name) => { *name == "car".to_string()}
-                _ => { false}
+    get_first_term(exp) == "car"
+}
+
+pub fn is_cdr(exp : &ast::SExpType) -> bool {
+    get_first_term(exp) == "cdr"
+}
+
+pub fn is_cons(exp : &ast::SExpType) -> bool {
+    get_first_term(exp) == "cons"
+}
+
+pub fn is_list(exp : &ast::SExpType) -> bool {
+    get_first_term(exp) == "list"
+}
+
+pub fn is_lambda(exp : &ast::SExpType) -> bool {
+    get_first_term(exp) == "lambda"
+}
+
+pub fn get_first_term(exp: &ast::SExpType) -> String {
+    match *exp {
+        ast::SExpType::Exp(ref form) => {
+            match (*form)[0] {
+                ast::SExpType::Identifier(ref name) => { name.clone()}
+                _ => { "".to_string()}
             }
         }
-        _ => {false}
+        _ => { "".to_string()}
+    }
+}
+
+pub fn eval<'a>(env: &'a mut HashMap<String, Var>, exp: &ast::SExpType) -> Result<&'a IType, &'static str> {
+    match *exp {
+        ast::SExpType::Identifier(ref name) => 
+        {
+            let k = env.get(name);
+            if k.is_some() {
+                match *k.unwrap() {
+                    Var::Bound(ref s) => { Ok(s.clone())}
+                    Var::UnBound => { Err("unbound variable")}
+                }
+            } else {
+                Err("no such variable defined")
+            }
+        }
+        _ => { Err("have not implemented further")}
     }
 }
 
