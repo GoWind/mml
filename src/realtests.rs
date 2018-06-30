2b4b06afbb4eda5e5d33e9cfddc81d3f03ee0ba2
govindarajann <ngovind@protonmail.com>
2018-06-30 15:21:24 +0530

    changed name of tests file to realtests

--- src/realtests.rs
+++ src/realtests.rs
@@ -1,15 +1,24 @@
-
 #[cfg(test)]
-mod tests {
+mod realtests {
     use env;
+    use env::IType;
     use ast;
     use tokenizer;
     use std::rc::Rc;
+    use std::collections::HashMap;
+
+    fn str_to_eval(k: &'static str, env: &mut HashMap<String, Rc<IType>>) -> Result<Rc<IType>, &'static str> {
+        let tok_stream = tokenizer::parse_string(&k.to_string());
+        let ast        = ast::stream_to_ast(&tok_stream).unwrap();
+        return env::eval(env, &ast);
+    }
+
     #[test]
     fn test_env() {
-        let mut env = env::HashMap::new();
+        let mut env = env::make_env();
+        let t = str_to_eval("true", & mut env);
+        assert_eq!(*(t.unwrap()), IType::True);
         env.insert("a".to_string(), Rc::new(env::IType::Atom(":hohoho".to_string())));
-        env.insert(String::from("True"), Rc::new(env::IType::True));
         {
             let tok_stream = tokenizer::parse_string(&"a".to_string());
             let ast = ast::stream_to_ast(&tok_stream).unwrap();
@@ -34,7 +43,7 @@ mod tests {
         let ast4 = ast::stream_to_ast(&tok_stream_4).unwrap();
         let v = env::eval(&mut env, &ast4);
         assert_eq!(true, match v {
-                            Ok(s) => { assert_eq!(*s, env::IType::Atom(":b".to_string())); true}
+                            Ok(s) => { assert_eq!(*s, IType::Atom(":b".to_string())); true}
                             Err(g) =>{ false}
         });
 
